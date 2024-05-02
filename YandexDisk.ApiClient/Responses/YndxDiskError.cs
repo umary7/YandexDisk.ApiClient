6322dc5531cd879a5b4ff421588f7bad8fb8a61c
umary7 <maria@t100.group>
2024-05-02 10:25:02 +0300

    feat: include HTTP status code in YndxDiskError

--- YandexDisk.ApiClient/Responses/YndxDiskError.cs
+++ YandexDisk.ApiClient/Responses/YndxDiskError.cs
@@ -2,9 +2,23 @@
 
 public record YndxDiskError
 {
+    /// <summary>
+    ///     Error message.
+    /// </summary>
     public string Message { get; init; }
-    
+
+    /// <summary>
+    ///     Error description.
+    /// </summary>
     public string Description { get; init; }
-    
+
+    /// <summary>
+    ///     Error name.
+    /// </summary>
     public string Error { get; init; }
+
+    /// <summary>
+    ///     HTTP status code.
+    /// </summary>
+    public int Status { get; internal set; } = -1;
 }
